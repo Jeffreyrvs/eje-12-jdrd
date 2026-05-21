3b4e48d81c6ce2b6ce52ea24e8f414ef87d42be7
Jeffrey <xxjeffrey2006xx@gmail.com>
2026-05-20 18:26:37 -0600

    Validaciones con modal

--- src/app/components/productos/productos.ts
+++ src/app/components/productos/productos.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ProductoModel } from '../../models/producto.model';
 import { ProductoService } from '../../services/producto.service';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-productos',
@@ -24,6 +25,16 @@ export class Productos implements OnInit {
   // Saber si estamos en edición o guardar producto
   enEdicion = false;
 
+  // Para el mensaje de las validaciones
+  mensajeModal: string = '';
+  modal!: bootstrap.Modal; 
+
+  // Producto a eliminar
+  productoSeleccionado: ProductoModel | null = null;
+
+  // Para cancelar edicion
+  mostrarBoton = false;
+
   constructor(private productoService: ProductoService) {
     
   }
@@ -38,30 +49,66 @@ export class Productos implements OnInit {
   }
 
   guardar() {
-    if (this.enEdicion) {
-      this.productoService.update({
-        ...this.producto
-      });
-      this.limpiar();
-      this.enEdicion = false;
+    if (this.producto.nombre.length >= 3 && this.producto.precio > 0){
+      // Validar que tenga longitud minima de 3 letras y precio mayor que cero
+      if (this.enEdicion) {
+        this.productoService.update({
+          ...this.producto
+        });
+        this.limpiar();
+        this.enEdicion = false;
+        this.mostrarBoton = false;
+      } else {
+        this.productoService.save({
+          ...this.producto
+        });
+        this.limpiar;
+      }
+    } else if (this.producto.nombre.length < 3 && this.producto.precio > 0) {
+      this.mensajeModal = 'El nombre debe tener una longitud minima de 3 caracteres.';
+      const elementoModal = document.getElementById('modal');
+
+      this.modal = new bootstrap.Modal(elementoModal!);
+
+      this.modal.show();
     } else {
-      this.productoService.save({
-        ...this.producto
-      });
-      this.limpiar;
+      this.mensajeModal = 'El precio debe ser mayor que cero.';
+      const elementoModal = document.getElementById('modal');
+
+      this.modal = new bootstrap.Modal(elementoModal!);
+
+      this.modal.show();
     }
+
   }
 
   actualizar(productoActualizar: ProductoModel) {
+    this.mostrarBoton = true;
     this.enEdicion = true;
     this.producto = {
       ...productoActualizar
     }
   }
 
+  cancelarEdicion() {
+    this.limpiar();
+    this.enEdicion = false;
+    this.mostrarBoton = false;
+  }
+
   eliminar(id: number) {
     this.productoService.delete(id);
     this.listar();
+    this.modal.hide();
+  }
+
+  mostrarConfirmacionEliminar(productoEliminar: ProductoModel){
+    this.productoSeleccionado = productoEliminar;
+    const elementoModal = document.getElementById('modalEliminar');
+
+    this.modal = new bootstrap.Modal(elementoModal!);
+
+    this.modal.show();
   }
 
   limpiar() {
